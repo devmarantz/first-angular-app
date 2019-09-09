d4425e059903fbc7465a41cb9b0dc919b1934f38
Devon Marantz <devonmarantz@gmail.com>
2019-09-09 11:05:28 -0700

    Change css to inline styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // styleUrls: ['./app.component.scss']
+  styles: [`
+    h2 {
+      color: dodgerblue;
+    }
+  `]
 })
 export class AppComponent {
 }
